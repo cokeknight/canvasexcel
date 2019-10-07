377d4bea29ae183766dc608037772d5757b00097
cokeknight <cokeknight1@163.com>
2019-10-07 17:59:03 +0800

    add

---
+++ public/jlb.js
@@ -0,0 +1,12 @@
+function run(){
+    console.time();
+     
+    for (var i = 0; i < 1000000; ++i) {
+     
+    }
+    console.timeEnd();
+    }
+
+    for (var i = 0; i < 200; ++i) {
+        run()
+       }
---
+++ src/columns.js
@@ -0,0 +1,87 @@
+export default [
+    {
+      name: 'core_name',
+      title: '所属中心',
+      fixed: 'left',
+      sortable: false,
+      width: 120
+    },
+    {
+      name: 'business_divisions_name',
+      title: '所属事业部',
+      fixed: 'left',
+      sortable: false,
+      width: 120
+    },
+    {
+      name: 'department_name',
+      title: '所属项目部',
+      sortable: false,
+      fixed: 'left'
+    }, {
+      name: 'product_name',
+      title: '产品',
+      sortable: false,
+      fixed: 'left',
+      width: 150
+    }, {
+      name: 'title',
+      title: '相关业务项目',
+      sortable: false,
+      fixed: 'left'
+    }, {
+      name: 'desc',
+      title: '备注说明',
+      sortable: false,
+      fixed: 'left'
+    },{
+      name: 'core_name',
+      title: '所属中心',
+      fixed: 'left',
+      sortable: false,
+      width: 120
+    },
+    {
+      name: 'business_divisions_name',
+      title: '所属事业部',
+      fixed: 'left',
+      sortable: false,
+      width: 120
+    },
+    {
+      name: 'department_name',
+      title: '所属项目部',
+      sortable: false,
+      fixed: 'left'
+    }, {
+      name: 'product_name',
+      title: '产品',
+      sortable: false,
+      fixed: 'left',
+      width: 150
+    }, {
+      name: 'title',
+      title: '相关业务项目',
+      sortable: false,
+      fixed: 'left'
+    }, {
+      name: 'desc',
+      title: '备注说明',
+      sortable: false,
+      fixed: 'left'
+    },{
+      name: 'core_name',
+      title: '所属中心',
+      fixed: 'left',
+      sortable: false,
+      width: 120
+    },
+    {
+      name: 'business_divisions_name',
+      title: '所属事业部',
+      fixed: 'left',
+      sortable: false,
+      width: 120
+    },
+    
+  ]
